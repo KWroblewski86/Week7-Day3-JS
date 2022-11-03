6569f5c291b52fc6fe97b7f47f8eafe5055366cc
Kyle Wroblewski <kyle.a.wroblewski@gmail.com>
2022-11-03 13:19:17 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// const res = fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+// .then((res) => {return res.json()})
+// .then((data) => {console.log(`Simple Fetch- `, data)})
+
+// const getPokemonFetch = async () => {
+//     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+//     const dat = await resp.json();
+//     console.log(`Async / await / fetch -`, dat);
+//     return dat
+// }
+// getPokemonFetch()
+
+
+let getData = async () => {
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+    if (response.status == 200){
+        console.log('Async / await / Axios', response.data)
+        return response.data
+    }
+    return 'API call is not working!'
+}
+getData()
+
+let loadData = async () => {
+    let data = await getData();
+    console.log(data)
+    let new_row = `<tr><td scope='row'>${data.results.name}</td></tr>`;
+    
+    document.getElementById("pokemonBody").insertAdjacentHTML('afterbegin', new_row);
+}
+
+let clearData = () => {
+    document.getElementById('pokemonBody').innerHTML='';
+}
+
